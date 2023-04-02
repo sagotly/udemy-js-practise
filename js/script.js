29cf3f3e1722c22d17c20cf203c0dc8f4b44ea70
tolik <laktolik9o@gmail.com>
2023-04-02 23:38:40 +0300

    firts practise

---
+++ js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {
+
+    },
+    genres: [],
+    private: false
+}
+
+personalMovieDB.movies[prompt('один из просмотреных фильмов?', '')] = prompt('насколько вы оцените его?', '');
+personalMovieDB.movies[prompt('один из просмотреных фильмов?', '')] = prompt('насколько вы оцените его?', '');
+
+console.log(personalMovieDB);
